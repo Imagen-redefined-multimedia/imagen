088f26dbe1f5a34dd94bde91cef014bff9762c8b
Zwane71 <makhohlisatemba@gmail.com>
2025-09-17 00:20:31 +0200

    Update layout.tsx
    fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ export const metadata: Metadata = {
   description: "High quality printing and branding solutions to elevate your business. Custom designs, eco-friendly printing materials, and fast delivery",
   keywords: "printing, wall art, branding, custom design, eco-friendly printing, business branding, business cards, steel fabrication, flyers, posters.",
   authors: [{name: "Imagen | Redefined multimedia"}],
-  viewport: "width=device-width, intitial-scale=1",
-  robot:"index, follow",
+  viewport: "width=device-width, initial-scale=1",
+  robots:"index, follow",
   openGraph: {
    title: "Imagen Redefined Multimedia | Printing And Branding Services",
    description: "High quality quality printing and branding solutions to elevate your business.",
