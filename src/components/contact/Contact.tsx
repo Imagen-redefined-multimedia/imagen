4e87a2c20f4fb38c14629ac2fcb44ddbee723929
Zwane71 <makhohlisatemba@gmail.com>
2025-09-17 00:26:57 +0200

    Update Contact.tsx
    fixed image properties

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import Image from "next/image";
 
 export default function ContactUs() {
+  return (
+    <>
+      <div className="text-center items-center">
+        <h1 className="text-[1.2rem] font-light">Get in Touch</h1>
+        <p className="font-light text-sm">
+          We would like to hear from you! If you have any questions, comments, or
+          feedback, please use the form below.
+        </p>
+      </div>
 
-return(
-	<>
-           <div className={" text-center item-center"}>
-	     <h1 className={" text-[1.2rem] font-light"}>Get in Touch </h1>
-	     <p className={"font-light text-sm"}> We would like to hear from you! If youhave any questions,comments or
- feedback. Please use the form below </p>
-	   </div>
-		<div className={"flex items-center py-3"}>
-		   <Image src={"./assets/wallpaper.svg"}
-			  alt={"Splash ArtWork"}
-			  width={400}
-			  height={200}
-			  aspectratio={"auto"} 
-			  className={"h-auto w-200 md:w-400"}/>
-		</div>
-	</>
-)}
+      <div className="flex justify-center py-3">
+        <div className="w-[400px] md:w-[400px] aspect-[2/1]">
+          <Image
+            src="/assets/wallpaper.svg"
+            alt="Splash ArtWork"
+            width={400}
+            height={200}
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
